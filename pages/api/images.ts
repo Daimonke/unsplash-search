4100c5427176e7e3d3a1dd5fb57b65b40ae065f2
Daimonke <Daimonas98@gmail.com>
2022-09-22 13:51:08 +0300

    Get images api handler

---
+++ pages/api/images.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const page = req.query.page ? req.query.page : "1";
+    const searchQuery = req.query.searchQuery;
+    const { data } = await axios.get(
+      `https://api.unsplash.com/search/photos?page=${page}&query=${searchQuery}&client_id=${process.env.client_id}`
+    );
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
