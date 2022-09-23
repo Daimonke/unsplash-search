33d5cfaab354c4041464bbe053063e87ba03f951
Daimonke <Daimonas98@gmail.com>
2022-09-23 16:03:44 +0300

    GridItem component

---
+++ components/main/GridItem.tsx
@@ -0,0 +1,25 @@
+import Image, { ImageProps } from "next/image";
+import { Tphoto } from "../../context/PhotosCtx";
+
+type Props = {
+  item: Tphoto;
+  options?: ImageProps;
+};
+
+const GridItem = ({ item, ...options }: Props) => {
+  return (
+    <Image
+      objectFit="cover"
+      src={item.urls.regular}
+      alt={item.description}
+      width={400}
+      height={400}
+      blurDataURL={item.urls.thumb}
+      placeholder="blur"
+      priority
+      {...options}
+    />
+  );
+};
+
+export default GridItem;
